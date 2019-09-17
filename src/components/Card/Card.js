77d470400e26d00a526806dbed83df298e01740d
Lucy Barker <lucy@geniemouse.com>
2019-09-17 17:29:11 +0100

    Add card flip animation functionality

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import CardFront from "./CardFront/CardFront";
 import CardBack from "./CardBack/CardBack";
@@ -6,12 +6,23 @@ import "./Card.css";
 
 function Card(props) {
     const { id, img, name, type } = props;
+    const [isFrontCard, setCardState] = useState(true);
+    let cardStateCss = "card card--showfront";
+
+    if (!isFrontCard) {
+        cardStateCss = "card card--showback";
+    }
+
     return (
-        <article id={`pokemon-${name}`} className="card">
+        <article id={`pokemon-${name}`} className={cardStateCss}>
             <div className="card-flip">
                 <CardFront id={id} img={img} name={name} type={type} />
                 <CardBack />
             </div>
+
+            <button className="flipcard-toggle" onClick={() => setCardState((prevState) => !prevState)}>
+                <span className="visually-hidden">Flip {name} card</span>
+            </button>
         </article>
     );
 }
