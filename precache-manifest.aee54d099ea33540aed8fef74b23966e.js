b2d4c7b2d19eb73b1dea8ab848fee2afface498c
Lucy Barker <lucy@geniemouse.com>
2019-09-18 12:41:11 +0100

    Updates

---
+++ precache-manifest.aee54d099ea33540aed8fef74b23966e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac2b1d1e9b5f8d3f8de8e7f6182260f",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "240ade5e1e9ccd5ce566",
+    "url": "/pokedex/static/css/main.197fa350.chunk.css"
+  },
+  {
+    "revision": "e203b37f158f16bf563d",
+    "url": "/pokedex/static/js/2.2d92ada4.chunk.js"
+  },
+  {
+    "revision": "240ade5e1e9ccd5ce566",
+    "url": "/pokedex/static/js/main.0905d74a.chunk.js"
+  },
+  {
+    "revision": "e2590d326f0902d7440b",
+    "url": "/pokedex/static/js/runtime~main.9b58ce06.js"
+  }
+]);
